47474aea6c7e7a7d332219d4bbc017e6f724d885
SentientSchnitzel <simondanielschneider@gmail.com>
2023-05-31 16:30:07 +0200

    training loop progress

--- train_eval.py
+++ train_eval.py
@@ -12,10 +12,12 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, Dataset
 
+from tqdm import tqdm
+
 from sklearn.preprocessing import RobustScaler, MinMaxScaler
 
 from preprocessing_utils import DF2Tensors, ScaleData, FetchDataLoader
-print("Modules loaded.")
+tqdm.write("Modules loaded.")
 
 ##### MODEL
 ## Define model architecture
@@ -53,16 +55,19 @@ class LSTM(nn.Module):
 ## Setup trainer function
 def train_model(model, train_loader, test_loader, criterion, optimizer, scheduler, pytorch_device,
                 num_epochs=50, n_epochs_stop=10, min_val_loss=np.Inf, epochs_no_improve=0, 
-                model_save=True, verbose=True, early_stopping=True):
+                model_save=True, save_dir='', verbose=True, early_stopping=True):
     
+    os.makedirs(save_dir, exist_ok=True)
     epoch_val_losses = []
     epoch_train_losses = []
 
-    for epoch in range(num_epochs):
+    pbar = tqdm(range(num_epochs), desc="Training", leave=True, unit='epochs')
+    for epoch in pbar:
         model.train()
 
         train_losses = []
-        for batch_X, batch_y in train_loader:
+        pbar_inner = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False, unit='batches')
+        for batch_X, batch_y in pbar_inner:
             batch_X = batch_X.to(pytorch_device)
             batch_y = batch_y.to(pytorch_device)
 
@@ -94,18 +99,18 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, schedule
                 val_losses.append(val_loss)
         val_loss = np.mean(val_losses)
         epoch_val_losses.append(val_loss)
+        
+        pbar.set_postfix({'Train Loss': train_loss, 'Val Loss': val_loss, 'Min Val Loss': min_val_loss})
 
-        # progress metering
-        if epoch % 5 == 0 and verbose:
-            print(f"Epoch {epoch+1}/{num_epochs}.. Train loss: {train_loss:.3f}, Validation loss: {val_loss:.3f}")
-
+        ## Utilities (saving/progress/early stopping)
         # check if validation loss has decreased (significantly)
-        if val_loss < min_val_loss and abs(min_val_loss - val_loss) > 0.001:
-            print(f'Validation loss decreased ({min_val_loss:.3f} --> {val_loss:.3f}).') if verbose else None
+        if min_val_loss > val_loss and abs(min_val_loss - val_loss) > 0.001:
+            tqdm.write(f'Validation loss decreased ({min_val_loss:.3f} --> {val_loss:.3f}).') if verbose else None
+            
             if model_save == True: # we save model and optimizer for the best validation loss
-                print('Saving model and optimizer state...') if verbose else None
-                torch.save(model.state_dict(), 'model.pt')
-                torch.save(optimizer.state_dict(), 'optimizer.pt')
+                tqdm.write('Saving model and optimizer state...') if verbose else None
+                torch.save(model.state_dict(), f'{save_dir}/model.pt')
+                torch.save(optimizer.state_dict(), f'{save_dir}/optimizer.pt')
 
             epochs_no_improve = 0
             min_val_loss = val_loss
@@ -113,13 +118,37 @@ def train_model(model, train_loader, test_loader, criterion, optimizer, schedule
             epochs_no_improve += 1
             # check if early stopping conditions are met
             if epochs_no_improve == n_epochs_stop and early_stopping == True:
-                print('Early stopping!')
+                tqdm.write('Early stopping!')
                 break
+        
 
         # step the scheduler
         scheduler.step(val_loss)
 
-        print("\nTraining reached end, consider increasing the number of epochs.") if epoch == num_epochs-1 else None
+        tqdm.write("\nTraining reached end, consider increasing the number of epochs.") if epoch == num_epochs-1 else None
+
+
+    # Save model hyperparameters
+    with open(f'{save_dir}/model_params.json', 'w') as f:
+        json.dump({'input_size': input_size, 
+                'hidden_layer_size': hidden_layer_size, 
+                'num_layers': num_layers, 
+                'output_size': output_size, 
+                'dropout_rate': dropout_rate}, 
+                f)
+
+    # helper hyperparameters
+    with open(f'{save_dir}/util_params.json', 'w') as f:
+        json.dump({'lr': lr, 
+                'patience': patience, 
+                'num_epochs': num_epochs}, 
+                f)
+    
+    # Save the losses for the current ticker
+    with open(f'{save_dir}/model_losses.json', 'w') as f:
+        json.dump({'train_losses': epoch_train_losses, 
+                'val_losses': epoch_val_losses}, 
+                f)
 
     return epoch_train_losses, epoch_val_losses, min_val_loss
 
@@ -129,11 +158,11 @@ def init_model(input_size, hidden_layer_size, num_layers, output_size, dropout_r
     # init model
     model = LSTM(input_size, hidden_layer_size, num_layers, output_size, dropout_rate)
     model.to(pytorch_device)
-    # print(model)
+    # tqdm.write(model)
 
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
-    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=patience, verbose=True) if scheduler else None
+    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=patience) if scheduler else None
 
     return model, criterion, optimizer, scheduler
 
@@ -154,7 +183,7 @@ if __name__ == "__main__":
             torch.cuda.manual_seed(seed)
 
     start_time = time.time()
-    print("Data load...")
+    tqdm.write("Data load...")
     # load csv
     raw_stock_data = pd.read_csv('data/SP500_stock_prices_log_clean_3monthreturn.csv', index_col=0, parse_dates=True)
     stock_tickers = pd.Series(raw_stock_data.Ticker.unique())
@@ -163,7 +192,7 @@ if __name__ == "__main__":
     float_cols = list(raw_stock_data.select_dtypes(include=['float64', 'int64']))
     raw_stock_data[float_cols] = raw_stock_data[float_cols].astype('float32')
     end_time = time.time()
-    print(f"Data load complete. Time elapsed: {end_time - start_time} seconds.")
+    tqdm.write(f"Data load complete. Time elapsed: {end_time - start_time:.2f} seconds.")
 
 
     input_size = 6
@@ -175,7 +204,7 @@ if __name__ == "__main__":
     patience = 3
 
     # Early stopping details
-    n_epochs_stop = 10
+    n_epochs_stop = 5
     min_val_loss = np.Inf
     epochs_no_improve = 0
 
@@ -203,14 +232,14 @@ if __name__ == "__main__":
         for ticker in stock_tickers:
             ticker_time = time.time()
 
-            print(f"Training model for {ticker} on fold {fold}.")
+            tqdm.write(f"Training model for {ticker} on fold {fold}.")
             # Scaling all data for the fold split. Turn them into tensors
             ticker_data = raw_stock_data[raw_stock_data.Ticker == ticker]
             scaled_ticker_data, ticker_scaler = ScaleData(df=ticker_data, split_date=split_date, scaler=MinMaxScaler())
             fold_scalers[ticker] = ticker_scaler
             
             # sequencial dict of tensors with train- and test-features and -targets
-            sequencial_tensors = DF2Tensors(scaled_ticker_data, split_date=split_date)
+            sequencial_tensors = DF2Tensors(scaled_ticker_data.drop(columns=['Ticker', 'Sector']), split_date=split_date)
             
             train_loader, test_loader = FetchDataLoader(sequencial_tensors, batch_size=16, num_workers=0)
 
@@ -224,6 +253,8 @@ if __name__ == "__main__":
                                                                 pytorch_device, 
                                                                 scheduler=True)
                 
+            save_dir = f'models/fold{fold}/{ticker}'
+            
             # train tha model
             epoch_train_losses, epoch_val_losses, min_val_loss = train_model(model=model,
                                                                             train_loader=train_loader, 
@@ -233,43 +264,15 @@ if __name__ == "__main__":
                                                                             scheduler=scheduler,
                                                                             pytorch_device=pytorch_device,
                                                                             num_epochs=num_epochs,
-                                                                            model_save=False, 
+                                                                            model_save=True,
+                                                                            save_dir=save_dir, 
                                                                             verbose=True, 
                                                                             early_stopping=True)
 
-            # Common directory path for saving model, optimizer, losses, and hyperparameters
-            save_dir = f'models/{fold}/{ticker}'
-            os.makedirs(save_dir, exist_ok=True)
-
-            # Save the model and hyperparameters
-            torch.save(model.state_dict(), f'{save_dir}/model.pt')
-            with open(f'{save_dir}/model_params.json', 'w') as f:
-                json.dump({'input_size': input_size, 
-                        'hidden_layer_size': hidden_layer_size, 
-                        'num_layers': num_layers, 
-                        'output_size': output_size, 
-                        'dropout_rate': dropout_rate}, 
-                        f)
-
-            # optimizer
-            torch.save(optimizer.state_dict(), f'{save_dir}/optimizer.pt')
-            with open(f'{save_dir}/util_params.json', 'w') as f:
-                json.dump({'lr': lr, 
-                        'patience': patience, 
-                        'num_epochs': num_epochs}, 
-                        f)
-
-
-            # Save the losses for the current ticker
-            with open(f'{save_dir}/model_losses.json', 'w') as f:
-                json.dump({'train_losses': epoch_train_losses, 
-                        'val_losses': epoch_val_losses}, 
-                        f)
-            
-            print(f"Finished training for {ticker} on fold {fold}. Time of training: {time.time() - ticker_time:.2f} seconds.")
+            tqdm.write(f"Finished training for {ticker} on fold {fold}. \nTime of training: {time.time() - ticker_time:.2f} seconds.\n")
         
         scalers[fold] = fold_scalers
         
-        print(f"Finished training for fold {fold}. Time for fold: {time.time() - fold_time:.2f} seconds.")
+        tqdm.write(f"Finished training for fold {fold}. \nTime for fold: {time.time() - fold_time:.2f} seconds.\n")
 
-    print(f"Finished training and validation for all folds. Elapsed time: {time.time() - start_time:.2f} seconds.")
+    tqdm.write(f"{'-'*30}\nFinished training and validation for all folds. \nElapsed time: {time.time() - start_time:.2f} seconds. \n{'-'*30}")
