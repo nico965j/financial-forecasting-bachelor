dfc470359fa5bb2d3c32331ad4d1a5d3a2e90aab
SentientSchnitzel <simondanielschneider@gmail.com>
2023-06-17 13:49:08 +0200

    deletion and renaming

--- train_gpu.sh
+++ train_gpu.sh
@@ -1,24 +1,24 @@
 #!/bin/sh 
 #BSUB -q gpuv100
-#BSUB -J "model_6vars"
-#BSUB -n 1
+#BSUB -J "sent_full"
+#BSUB -n 4
 #BSUB -R "span[hosts=1]"
-#BSUB -R "rusage[mem=4GB]"
+#BSUB -R "rusage[mem=16GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -M 4GB
-### wall-time 4 hr
-#BSUB -W 4:00 
+#BSUB -M 20GB
+### wall-time 24 hr
+#BSUB -W 24:00 
 #BSUB -u simondanielschneider@gmail.com
 #BSUB -N
 
 ### Doesnt overwrite, just makes new. 
-#BSUB -o outputs/GPU_Output_%J.out 
-#BSUB -e errors/GPU_Error_%J.err
+#BSUB -o outputs/sent_FULL_Output_%J.out 
+#BSUB -e errors/sent_FULL_Error_%J.err
 
 ### Activate virtual environment
 source act-venv.sh
 
-module load cuda/11.0
+module load cuda/11.6
 
 ### Write which script to run below
-python3 train_eval.py
+python3 simple_sentiment.py
